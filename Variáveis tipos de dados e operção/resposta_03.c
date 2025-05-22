98b8cefbb6baeb7fc1f961681a7ed880c204abe6
VictorHugo <mgvitinhon1@gmail.com>
2025-05-22 18:20:31 -0300

    Create resposta_03.c
    criada a resposta do exercício 03 com a utilização da biblioteca "limits" para utilização do "INT_MAX"  na qual pega o maior numero inteiro que a linguagem c permite utilizar

---
+++ Variáveis tipos de dados e operção/resposta_03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>//adiciona variaveis que armazena numeros longos
+
+int main(){
+	int numeroMaximo=INT_MAX;//ira buscar o maior numero que o probrama possibilita
+	
+	printf("O numero maximo dado pelo programa eh:2*\n %d",numeroMaximo);
+	
+	return 0;
+	
+	
+}
